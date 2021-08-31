8713ee27e8a56e6c0158fee7114a65438c783564
Keith Walsh <kwalsh@redhat.com>
2021-08-31 13:05:34 -0400

    Specify logger per stream
    Since `watchtower` only gives us a handler, not an inherited logger object, we need
    to make sure we have a logger per stream, and use that per request. Otherwise we'll
    end up sending logs to any/all streams where handlers have been created against
    our main logger object.
    
    Since `logging.getLogger(name)` is idempotent, we can use it to get or create the
    logger per request, and add the handler if we haven't cached it.

--- app/log.py
+++ app/log.py
@@ -8,33 +8,52 @@ from flask import request, abort, jsonify, make_response
 from boto3.session import Session
 
 logging.basicConfig(level=os.getenv("LOG_LEVEL", logging.INFO))
-logger = logging.getLogger(__name__)
+
 
 class Cache:
     active_stream_handlers = {}
     allowed_streams = os.getenv("CLOUD_WATCH_ALLOWED_STREAMS", "").split(",")
 
+
 @app.route("/ping")
 def ping():
     return {"status": "available"}, 200
 
+
 @app.route("/log/<log_stream>", methods=["POST"])
 def log(log_stream):
     validate_log_stream(log_stream)
-    add_handler(log_stream)
+    logger = logging.getLogger(log_stream)
+    add_handler(log_stream, logger)
     logger.info(request.get_json())
     return {"status": "accepted"}, 202
 
+
 def validate_log_stream(log_stream):
     if log_stream not in Cache.allowed_streams:
-        abort(make_response(jsonify(message=f"{log_stream} is not a valid CloudWatch log stream."), 403))
+        abort(
+            make_response(
+                jsonify(message=f"{log_stream} is not a valid CloudWatch log stream."),
+                403,
+            )
+        )
 
-def add_handler(log_stream):
+
+def add_handler(log_stream, logger):
     if not Cache.active_stream_handlers.get(log_stream):
-        Cache.active_stream_handlers[log_stream] = True
-        logger.addHandler(watchtower.CloudWatchLogHandler(log_group=os.getenv("AWS_LOG_GROUP"), stream_name=log_stream, boto3_session=session()))
+        Cache.active_stream_handlers[log_stream] = logger
+        logger.addHandler(
+            watchtower.CloudWatchLogHandler(
+                log_group=os.getenv("AWS_LOG_GROUP"),
+                stream_name=log_stream,
+                boto3_session=session(),
+            )
+        )
+
 
 def session():
-    return Session(aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-            aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-            region_name=os.getenv("AWS_REGION_NAME"))
+    return Session(
+        aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+        region_name=os.getenv("AWS_REGION_NAME"),
+    )
