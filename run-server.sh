6fee5100f141230f51c014cfd5767f2c8eac3050
Jay Zeng <astrozzc@gmail.com>
2026-02-02 14:39:24 -0500

    Making the requests async

--- run-server.sh
+++ run-server.sh
@@ -4,5 +4,7 @@ if [ "x$FLASK_ENV" = "xdevelopment" ]
 then
   flask run --host=0.0.0.0 --reload
 else
-  gunicorn -w 4 -b 0.0.0.0:$PUBLIC_PORT app:app
+  # Use gevent workers for async I/O - allows handling many concurrent requests
+  # Each worker can handle hundreds of concurrent connections
+  gunicorn -w ${GUNICORN_WORKERS:-4} -k gevent --worker-connections ${GUNICORN_WORKER_CONNECTIONS:-1000} -b 0.0.0.0:$PUBLIC_PORT app:app
 fi
